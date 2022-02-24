80b6a6d9fcd579f35ab22aa8b74330b6a95ef229
Artemii-Belousov <neochoto@mail.ru>
2022-02-25 02:00:05 +0500

    lesson-3

---
+++ lesson-3/webpack.config.js
@@ -0,0 +1,81 @@
+const {
+  resolve
+} = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'mini.[contenthash].js'
+
+  },
+  module: {
+    rules: [{
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      // {
+      //     test: /\.(png|jpe?g|gif)$/i,
+      //     use: [
+      //         {
+      //             loader: 'file-loader',
+      //             options: {
+      //                 name: '[name].[ext]',  // Сохраняет исходное назване файла
+      //                 outputPath: './images'  // Указывает путь для экспорта файлов
+      //             }
+      //         }
+      //     ]
+      // },
+
+      // {
+      //     test: /\.(png|jpe?g|gif)$/i,
+      //     use: [
+      //         {
+      //             loader: 'image-optimize-loader',
+      //             options: {
+      //                 compress: {
+      //                     mode: 'high',
+      //                     webp: true,
+      //                     gifsicle: true,
+      //                     disableOnDevelopment: false,
+      //                 }
+      //             }
+      //         }
+
+      //     ]
+      // }
+
+      {
+        test: /\.(png|jpe?g|webp|git|svg|gif|)$/i,
+        use: [{
+          loader: 'img-optimize-loader',
+          options: {
+            compress: {
+              // This will take more time and get smaller images.
+              mode: 'high', // 'lossless', 'low'
+              disableOnDevelopment: true,
+              webp: true, // преобразует файлы в формат webp
+              gifsicle: {
+                optimizationLevel: 3, //сжатие для gif
+              },
+            },
+          },
+        }, ],
+      },
+      {
+        test: /\.(mp[3|4])$/i,
+        use: ['file-loader']
+      },
+    ]
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: resolve(__dirname, 'src', 'index.html')
+    })
+  ]
+}
